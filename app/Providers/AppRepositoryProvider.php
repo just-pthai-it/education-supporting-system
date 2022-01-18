ba8f0ee4d510f19e280b908e5ef006ef2b36d2cb
WRBKOR23 <pthai.it.210501@gmail.com>
2022-01-18 14:48:31 +0700

    refactor code

--- app/Providers/AppRepositoryProvider.php
+++ app/Providers/AppRepositoryProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Faculty;
-use App\Models\Department;
 use App\Repositories\RoomRepository;
 use App\Repositories\FeedbackRepository;
 use App\Repositories\PermissionRepository;
