936ea7446c6dcd1c294c7db1ff0d71624ddec0e2
WRBKOR23 <pthai.it.210501@gmail.com>
2022-02-02 23:01:10 +0700

    add mail notify to request change schedule feature

--- app/Repositories/Contracts/ScheduleRepositoryContract.php
+++ app/Repositories/Contracts/ScheduleRepositoryContract.php
@@ -7,4 +7,6 @@ interface ScheduleRepositoryContract extends BaseRepositoryContract
     public function findAllByIdTeacher ($id_teacher, $start, $end);
 
     public function findAllByIdDepartment ($id_department, $start, $end);
+
+    public function findTeacherEmailByIdSchedule (int $id_schedule);
 }
