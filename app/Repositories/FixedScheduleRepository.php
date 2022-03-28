694c271b464023b2cb4c5113ab6661bc141ffec8
WRBKOR23 <pthai.it.210501@gmail.com>
2022-03-28 17:38:05 +0700

    refactor code

--- app/Repositories/FixedScheduleRepository.php
+++ app/Repositories/FixedScheduleRepository.php
@@ -45,9 +45,9 @@ class FixedScheduleRepository extends BaseRepository implements Contracts\FixedS
                                            'schedule.moduleClass:id,name,id_teacher,number_reality',
                                            'schedule.moduleClass.teacher:id,name']);
 
-        if (isset($conditions['page']))
+        if (isset($conditions['page'], $conditions['pagination']))
         {
-            return $this->model->paginate($conditions['pagination'] ?? 20);
+            return $this->model->paginate($conditions['pagination']);
         }
 
         return $this->model->get();
