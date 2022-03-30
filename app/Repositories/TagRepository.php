c0eac8bfb56b0f4d6ffc673f0c3815ce52e0d590
WRBKOR23 <pthai.it.210501@gmail.com>
2022-03-30 20:20:47 +0700

    add tag repository to serve notification service

---
+++ app/Repositories/TagRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Tag;
+use App\Repositories\Abstracts\BaseRepository;
+
+class TagRepository extends BaseRepository implements Contracts\TagRepositoryContract
+{
+    function model () : string
+    {
+        return Tag::class;
+    }
+
+
+}
