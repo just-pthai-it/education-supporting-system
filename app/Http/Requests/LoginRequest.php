88effb4ddfb53b62e8e86abd0646c440f206e7fb
WRBKOR23 <pthai.it.210501@gmail.com>
2022-04-12 14:25:58 +0700

    refactor authentication

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     * @return bool
+     */
+    public function authorize () : bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * @return array
+     */
+    public function rules () : array
+    {
+        return [
+            'username' => 'required',
+            'password' => 'required|min:4'
+        ];
+    }
+}
