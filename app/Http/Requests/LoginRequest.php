39f6ce814644314ef4fa7ac2bc44ddae45779c9a
WRBKOR23 <pthai.it.210501@gmail.com>
2022-04-15 20:57:05 +0700

    add LoginRequest.php

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
+    protected $redirect = '/api/bad-request';
+
     /**
      * Determine if the user is authorized to make this request.
      * @return bool
